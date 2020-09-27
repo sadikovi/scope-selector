958851fd04ba53db30af2916d588304dff0dfe65
Ivan Sadikov <ivan.sadikov@gmail.com>
2020-09-27 08:04:52 +0200

    initial commit

---
+++ spec/scope-selector-view-spec.js
@@ -0,0 +1,9 @@
+'use babel';
+
+import ScopeSelectorView from '../lib/scope-selector-view';
+
+describe('ScopeSelectorView', () => {
+  it('has one valid test', () => {
+    expect('life').toBe('easy');
+  });
+});
